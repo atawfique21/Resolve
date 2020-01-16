b2c33fde09a31dcda4379de65a943f178b43d59e
atawfique12 <abirtawfique12@outlook.com>
2020-01-15 21:04:07 -0500

    Version one of header completed.

--- client/src/Components/header.js
+++ client/src/Components/header.js
@@ -3,9 +3,16 @@ import React from 'react';
 export default function Header() {
   return (
     <header>
-      <h1>Resolve</h1>
-      <button>Login</button>
-      <button>Register</button>
+      <div className="header-container">
+        <div className="logo-desc">
+          <h1>Resolve</h1>
+          <h5>The social way to resolve goals</h5>
+        </div>
+        <div className="header-buttons-container">
+          <button>Login</button>
+          <button>Register</button>
+        </div>
+      </div>
     </header>
   )
 }
