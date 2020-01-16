4823417a3bf5b9fff2db81f78d7a5c622cb8c507
hannahreilly <reillyhannah65@gmail.com>
2020-01-16 10:01:08 -0500

    rendering login and register forms

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import './App.css';
+import LoginForm from './Components/LoginForm';
+import RegisterForm from './Components/RegisterForm';
 
 function App() {
   return (
     <div className="App">
+      <LoginForm />
+      <RegisterForm />
     </div>
   );
 }
