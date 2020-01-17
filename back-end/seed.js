a58522ef1341cb6a78b00fc3f33d5041f018ca3c
atawfique12 <abirtawfique12@outlook.com>
2020-01-17 10:59:34 -0500

    Changed password_digest to string format and corrected them to password of tokens.

--- back-end/seed.js
+++ back-end/seed.js
@@ -3,14 +3,9 @@ const { User, Goal } = require('./models');
 const seed = async () => {
   await User.destroy({ where: {} })
 
-  const admin = await User.create({
-    username: "casey",
-    password_digest: "$2b$11$J/T6HO7/IHF7TXgu4elqfO.YW6PJIOS9lQdmTQ789Sg7GUPkIer.O"
-  })
-
   const user1 = await User.create({
     username: "hannister01",
-    password_digest: 1234,
+    password_digest: "$2b$11$QYdYrBQI8FQKUShSfd1urOf/1hGqWdMLIvs2rdhbj9Bq5h8m.IZiS",
     first_name: "Hannah",
     last_name: "Reilly",
     fun_fact: "My name is a palidrone.",
@@ -20,7 +15,7 @@ const seed = async () => {
 
   const user2 = await User.create({
     username: "atawfique21",
-    password_digest: 1234,
+    password_digest: "$2b$11$QYdYrBQI8FQKUShSfd1urOf/1hGqWdMLIvs2rdhbj9Bq5h8m.IZiS",
     first_name: "Abir",
     last_name: "Tawfique",
     fun_fact: "My brother and I were born on the same day but four years apart.",
@@ -30,7 +25,7 @@ const seed = async () => {
 
   const user3 = await User.create({
     username: "revatir",
-    password_digest: 1234,
+    password_digest: "$2b$11$QYdYrBQI8FQKUShSfd1urOf/1hGqWdMLIvs2rdhbj9Bq5h8m.IZiS",
     first_name: "Revati",
     last_name: "Rajabhathor",
     fun_fact: "Since joining GA I eat an average of 2 cups of goldfish a day.",
