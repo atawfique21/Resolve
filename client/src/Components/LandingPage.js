78f50e68ebc1f816308944e5bcc2f0eb1abd9c55
atawfique12 <abirtawfique12@outlook.com>
2020-01-17 15:34:37 -0500

    Changed text for product description.

--- client/src/Components/LandingPage.js
+++ client/src/Components/LandingPage.js
@@ -6,7 +6,7 @@ export default function LandingPage() {
     <div className="landingpage-wrapper">
       <div className="landingpage-content">
         <img src="https://designshack.net/wp-content/uploads/placeholder-image.png" alt="image of application"></img>
-        <h3>Resolve helps you <span className="blue-highlight">create</span>, <span className="blue-highlight">track</span> and <span className="blue-highlight">share</span> personal and professional goals.</h3>
+        <h3>Resolve helps you <span className="blue-highlight">track</span> & <span className="blue-highlight">share</span> personal/professional goals.</h3>
         <div className="header-buttons-container redirect">
           <Link to="/register">Become a Resolver Now</Link>
         </div>
