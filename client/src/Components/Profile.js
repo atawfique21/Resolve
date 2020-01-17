2b23ef6ed0111c50ee96f8111bb15bcdcb94f69b
hannahreilly <reillyhannah65@gmail.com>
2020-01-17 15:03:03 -0500

    updated header on profile page

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import axios from 'axios';
-import { Link } from 'react-router-dom';
 
 class Profile extends Component {
     constructor(props) {
@@ -12,6 +11,7 @@ class Profile extends Component {
             profile_pic_url: 'https://media-exp1.licdn.com/dms/image/C4D03AQGmzrAJOFCUBA/profile-displayphoto-shrink_200_200/0?e=1584576000&v=beta&t=Jdi_jghFaIqdNWivMFf6zFDLrpicS77YnD2d9E2Dqgk',
             fun_fact: 'My name is a palidrone',
             location:'NYC',
+            pinpoint:'https://image.flaticon.com/icons/png/512/67/67347.png',
             apiDataLoaded: false
         }
     };
@@ -31,11 +31,17 @@ class Profile extends Component {
     render() {
         return (
             <div className="profile">
-                <h1>{this.state.first_name}</h1>
-                <h1>{this.state.last_name}</h1>
-                <img src={this.state.profile_pic_url} alt="pic"></img>
+                <img src={this.state.profile_pic_url} alt="profile"></img>
+                
+                <div className="names">
+                <h1>{this.state.first_name} {this.state.last_name}</h1>
                 <h3>{this.state.fun_fact}</h3>
+                </div>
+
+                <div className="location">
+                <img className="pinpoint" src={this.state.pinpoint} alt="map"></img>
                 <h4>{this.state.location}</h4>
+                </div>
             </div>
         )
     }
