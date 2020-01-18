97e5139c8f760662680f31c52018e52c54610fa7
Revati Rajabhathor <revatirajabhathor@yahoo.com>
2020-01-18 12:21:17 -0500

    [Feed.js] Removed useless constructor

--- client/src/Components/Feed.js
+++ client/src/Components/Feed.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Feed extends Component {
-  constructor(props) {
-    super(props);
-
-  }
-
   render() {
     const linkStyle = {
       textDecoration: "none",
