8da921a98d0fc3def200fe2d81ef4efaeb303217
A Tawfique <easternselling@gmail.com>
2020-01-20 16:33:49 -0500

    Added search functionaliuty

--- client/src/Components/Feed.js
+++ client/src/Components/Feed.js
@@ -3,13 +3,17 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import AddGoal from './AddGoal'
 import { createGoal } from '../Services/apiHelper'
+import Search from './Search'
 
 class Feed extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      users: []
+      users: false,
+      apiDataLoaded: false,
+      search: null,
+      filteredUsers: []
     }
   }
 
@@ -17,6 +21,39 @@ class Feed extends Component {
     e.preventDefault();
     const currentGoal = await createGoal(sentGoal);
     console.log(currentGoal)
+  }
+
+  componentDidMount = async () => {
+    try {
+      const userResponse = await axios(`http://localhost:3001/auth`);
+      this.setState({
+        users: userResponse.data,
+        apiDataLoaded: true
+      })
+      console.log(this.state.users)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  handleSearchChange = async (e) => {
+    const { value } = e.target;
+    await this.setState({
+      search: value
+    })
+    this.filterUsers()
+  }
+
+  filterUsers = () => {
+    let filteredUsers = this.state.users.filter(user =>
+      user.first_name.toLowerCase().startsWith(this.state.search.toLowerCase())
+      // this.state.search.toLowerCase().startsWith(user.first_name.toLowerCase())
+      // this.state.search.toLowerCase() === user.first_name.toLowerCase()
+    )
+    this.setState({
+      filteredUsers
+    })
+    console.log(this.state.filteredUsers)
   }
 
   render() {
@@ -27,9 +64,20 @@ class Feed extends Component {
     return (
       <div className="feedpage-wrapper">
         <AddGoal handleAdd={this.handleAdd} />
+        <Search handleChange={this.handleSearchChange} />
         <div className="feedpage-content">
-          {this.props.apiDataLoaded &&
-            this.props.users.map(user => (
+          {this.state.apiDataLoaded && this.state.search &&
+            this.state.filteredUsers.map(user => (
+              <Link style={linkStyle} to={`/profile/${user.id}`}>
+                <div key={user.id} className="users">
+                  <h4>{user.first_name}</h4>
+                  <img src={user.profile_pic_url} alt="user" />
+                </div>
+              </Link>
+            ))
+          }
+          {this.state.apiDataLoaded && !this.state.search &&
+            this.state.users.map(user => (
               <Link style={linkStyle} to={`/profile/${user.id}`}>
                 <div key={user.id} className="users">
                   <h4>{user.first_name}</h4>
