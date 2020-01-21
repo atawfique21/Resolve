b1bf324682e7a080f8f801c8b2e326be32b5bd0a
A Tawfique <easternselling@gmail.com>
2020-01-21 12:19:47 -0500

    Now conditionally rendering login text for logged out users.

--- client/src/Components/Profile.js
+++ client/src/Components/Profile.js
@@ -111,51 +111,58 @@ export default class Profile extends Component {
   render() {
     return (
       <div>
-        {this.state.user && (
+        {!this.props.currentUser ?
+          <div className="restrict">
+            <h1>Please <a href="/login">login</a> to see this profile.</h1>
+          </div> :
           <div>
-            <div className="profile">
-              <img src={this.state.user.profile_pic_url} alt="profile" className="profile-image"></img>
+            {this.state.user && (
+              <div>
+                <div className="profile">
+                  <img src={this.state.user.profile_pic_url} alt="profile" className="profile-image"></img>
 
-              <section className="names">
-                <h1>{this.state.user.first_name} {this.state.user.last_name}</h1>
-                <h4 className="fun-fact">Fun Fact: "{this.state.user.fun_fact}"</h4>
-              </section>
+                  <section className="names">
+                    <h1>{this.state.user.first_name} {this.state.user.last_name}</h1>
+                    <h4 className="fun-fact">Fun Fact: "{this.state.user.fun_fact}"</h4>
+                  </section>
 
-              <section className="location">
-                <img className="pinpoint" src={this.props.pinpoint} alt="map"></img>
-                <h4>{this.state.user.location}</h4>
-              </section>
-            </div >
-            <div className="goals">
-              <h3 className="blue-highlight">{this.state.user.first_name}'s Goals</h3>
-              {this.state.goals.length > 0 ?
-                <div>
-                  <div className="goal-wrapper">
-                    {this.state.goals.map(goal => (
-                      <div className={goal.is_complete ? "complete single-goal" : "incomplete single-goal"} key={goal.id}>
-                        <h4 className="blue-highlight">{goal.name}</h4>
-                        <h5><span className="right">></span> My Motivation</h5>
-                        <p>{goal.motivation}</p>
-                        <h5><span className="right">></span> My Plan</h5>
-                        <p>{goal.plan}</p>
-                        <div className="task-buttons">
-                          <img src={completeicon} className="task-single-button" onClick={(e) => this.goalComplete(e, goal.id)}></img>
-                          <img src={editicon} className="task-single-button" id="edit-button"></img>
-                          <img src={deleteicon} className="task-single-button" onClick={(e) => this.confirmDelete(e, goal.id)}></img>
-                          {this.state.alert}
-                        </div>
+                  <section className="location">
+                    <img className="pinpoint" src={this.props.pinpoint} alt="map"></img>
+                    <h4>{this.state.user.location}</h4>
+                  </section>
+                </div >
+                <div className="goals">
+                  <h3 className="blue-highlight">{this.state.user.first_name}'s Goals</h3>
+                  {this.state.goals.length > 0 ?
+                    <div>
+                      <div className="goal-wrapper">
+                        {this.state.goals.map(goal => (
+                          <div className={goal.is_complete ? "complete single-goal" : "incomplete single-goal"} key={goal.id}>
+                            <h4 className="blue-highlight">{goal.name}</h4>
+                            <h5><span className="right">></span> My Motivation</h5>
+                            <p>{goal.motivation}</p>
+                            <h5><span className="right">></span> My Plan</h5>
+                            <p>{goal.plan}</p>
+                            <div className="task-buttons">
+                              <img src={completeicon} className="task-single-button" onClick={(e) => this.goalComplete(e, goal.id)}></img>
+                              <img src={editicon} className="task-single-button" id="edit-button"></img>
+                              <img src={deleteicon} className="task-single-button" onClick={(e) => this.confirmDelete(e, goal.id)}></img>
+                              {this.state.alert}
+                            </div>
+                          </div>
+                        ))}
                       </div>
-                    ))}
-                  </div>
-                  <p className="end">🏁 Those are all of {this.state.user.first_name}'s goals... for now!</p>
-                </div>
-                :
-                <h3 id="no-goals">{this.state.user.first_name} does not have any goals set up yet.</h3>
-              }
+                      <p className="end">🏁 Those are all of {this.state.user.first_name}'s goals... for now!</p>
+                    </div>
+                    :
+                    <h3 id="no-goals">{this.state.user.first_name} does not have any goals set up yet.</h3>
+                  }
 
-            </div>
+                </div>
+              </div>
+            )}
           </div>
-        )}
+        }
       </div>
     )
   }
